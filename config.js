ef9a020ae97109ddc29707c2254b864b7186e372
AZIHubert <allan.jouannet@protonmail.com>
2020-06-13 04:53:40 +0200

    add config for heroku

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	MONGODB: 'mongodb+srv://AZIHubert:Aaoudjiuvhds9@cluster0-v2zm2.mongodb.net/ACPLVDB?retryWrites=true&w=majority',
+	CLOUDINARY_API_KEY: '486169621137989',
+	CLOUDINARY_API_SECRET: 'A_nWpflwBkpumS1vBBk1fDNhhXQ',
+	CLOUDINARY_CLOUD_NAME: 'dfpujlj9q',
+	CLOUDINARY_UPLOAD_PRESET: 'ihs5jtgt',
+	SECRET_KEY: 'MQQBEoBgGjLCTMrTaTsB4RCakHhS',
+	OUTLOOK_EMAIL: 'acplv.emailsender@gmail.com',
+	OUTLOOK_PASSWORD: 'SjMSA98QgL4jfHFytntnyMYJ',
+	REGISTER_CONFIRMATION: 'NCEz6EgYkoMs6DqRrBpri9niPDRm'
+}
